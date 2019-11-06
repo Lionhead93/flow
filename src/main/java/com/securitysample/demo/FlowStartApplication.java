c98a30b889a9bdb247b72e0623e1e0d5d4f16566
seongwou <seongwou@hanpass.com>
2019-11-06 10:33:12 +0900

    Create

---
+++ src/main/java/com/securitysample/demo/FlowStartApplication.java
@@ -0,0 +1,13 @@
+package com.securitysample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowStartApplication.class, args);
+	}
+
+}
